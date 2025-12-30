d00a39561be531181faabf5ec5dee6bd61d4d461
Adrián Marín <mincho1811@gmail.com>
2025-12-30 15:11:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ document.getElementById("consultar").addEventListener("click", () => {
   const r = palabras[Math.floor(Math.random() * palabras.length)];
   document.getElementById("respuesta").innerText = r;
 });
+const respuesta = document.getElementById("respuesta");
+respuesta.style.opacity = 0;
+
+setTimeout(() => {
+  respuesta.innerText = r;
+  respuesta.style.opacity = 1;
+}, 300);
